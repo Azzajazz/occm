c11fc2ede2fa0e279280015cbc52bc394cd659af
Adam Rowland <u5799578@anu.edu.au>
2024-10-20 07:50:55 +1100

    Support for single test cases

--- test/run_tests.py
+++ test/run_tests.py
@@ -7,91 +7,109 @@ import exp_files
 def is_case_of(dirpath: str, case: str):
     return f"\\{case}" in dirpath
 
-def do_tests(dirname: str):
+def do_valid_test(source_path: str):
     global passed
     global failed
 
+    print(f"Running test {source_path}:  ", end = "")
+    compile_result = subprocess.run(
+            ["../occm.exe", source_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+    if compile_result.returncode != 0:
+        print("FAILED! Compilation failed")
+        failed += 1
+        return
+
+    exec_path = os.path.basename(source_path)[:-2] + ".exe"
+    exp_file_path = exp_files.exp_file_path_from_source_path(source_path)
+    exp_file = exp_files.ExpFile(exp_file_path)
+    run_result = subprocess.run(
+            [exec_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+    if exp_file.exit_code == run_result.returncode:
+        print("PASSED!")
+        passed += 1
+    else:
+        print(f"FAILED! Return codes do not match. Expected {exp_file.exit_code}, got {run_result.returncode}")
+        failed += 1
+    os.remove(exec_path)
+
+def do_invalid_lex_test(source_path: str):
+    global passed
+    global failed
+
+    print(f"Running test {source_path}:  ", end = "")
+    compile_result = subprocess.run(
+            ["../occm.exe", source_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+    if compile_result.returncode == 2:
+        print("PASSED!")
+        passed += 1
+    else:
+        print("FAILED! Lex should not have succeeded")
+        failed += 1
+
+def do_invalid_parse_test(source_path: str):
+    global passed
+    global failed
+
+    print(f"Running test {source_path}:  ", end = "")
+    compile_result = subprocess.run(
+            ["../occm.exe", source_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )               
+    if compile_result.returncode == 3:
+        print("PASSED!")
+        passed += 1
+    else:
+        print("FAILED! Parse should not have succeeded")
+        failed += 1
+
+def do_invalid_semantics_test(source_path: str):
+    global passed
+    global failed
+
+    print(f"Running test {source_path}:  ", end = "")
+    compile_result = subprocess.run(
+            ["../occm.exe", source_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )               
+    if compile_result.returncode == 4:
+        print("PASSED!")
+        passed += 1
+    else:
+        print("FAILED! Semantics are incorrect")
+        failed += 1
+
+def do_test(dirname: str, source_path: str):
+    if is_case_of(dirname, "valid"):
+        do_valid_test(source_path)
+
+    elif is_case_of(dirname, "invalid_lex"):
+        do_invalid_parse_test(source_path)
+
+    elif is_case_of(dirname, "invalid_parse"):
+        do_invalid_parse_test(source_path)
+
+    elif is_case_of(dirname, "invalid_semantics"):
+        do_invalid_semantics_test(source_path)
+
+def do_tests(dirname: str):
+    if os.path.isfile(dirname):
+        do_test(dirname, dirname)
+
     for root, dirs, files in os.walk(dirname):
-        if is_case_of(root, "valid"):
-            for file in filter(lambda f: f.endswith(".c"), files):
-                source_path = os.path.join(root, file)
-
-                print(f"Running test {source_path}:  ", end = "")
-                compile_result = subprocess.run(
-                        ["../occm.exe", source_path],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL
-                    )
-                if compile_result.returncode != 0:
-                    print("FAILED! Compilation failed")
-                    failed += 1
-                    continue
-
-                exec_path = file[:-2] + ".exe"
-                exp_file_path = exp_files.exp_file_path_from_source_path(source_path)
-                exp_file = exp_files.ExpFile(exp_file_path)
-                run_result = subprocess.run(
-                        [exec_path],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL
-                    )
-                if exp_file.exit_code == run_result.returncode:
-                    print("PASSED!")
-                    passed += 1
-                else:
-                    print(f"FAILED! Return codes do not match. Expected {exp_file.exit_code}, got {run_result.returncode}")
-                    failed += 1
-                os.remove(exec_path)
-
-        elif is_case_of(root, "invalid_lex"):
-            for file in filter(lambda f: f.endswith(".c"), files):
-                source_path = os.path.join(root, file)
-                print(f"Running test {source_path}:  ", end = "")
-                compile_result = subprocess.run(
-                        ["../occm.exe", source_path],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL
-                    )
-                if compile_result.returncode == 2:
-                    print("PASSED!")
-                    passed += 1
-                else:
-                    print("FAILED! Lex should not have succeeded")
-                    failed += 1
-
-        elif is_case_of(root, "invalid_parse"):
-            for file in filter(lambda f: f.endswith(".c"), files):
-                source_path = os.path.join(root, file)
-                print(f"Running test {source_path}:  ", end = "")
-                compile_result = subprocess.run(
-                        ["../occm.exe", source_path],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL
-                    )               
-                if compile_result.returncode == 3:
-                    print("PASSED!")
-                    passed += 1
-                else:
-                    print("FAILED! Parse should not have succeeded")
-                    failed += 1
-
-        elif is_case_of(root, "invalid_semantics"):
-            for file in filter(lambda f: f.endswith(".c"), files):
-                source_path = os.path.join(root, file)
-                print(f"Running test {source_path}:  ", end = "")
-                compile_result = subprocess.run(
-                        ["../occm.exe", source_path],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL
-                    )               
-                if compile_result.returncode == 4:
-                    print("PASSED!")
-                    passed += 1
-                else:
-                    print("FAILED! Semantics are incorrect")
-                    failed += 1
-
-
+        for file in filter(lambda f: f.endswith(".c")):
+            source_path = os.path.join(root, file)
+            do_test(root, source_path)
 
 def main():
     parser = argparse.ArgumentParser()
